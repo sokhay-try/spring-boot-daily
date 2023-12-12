b5a5d2ed2e13ffabf11953c7a1d51ac6789dac2c
Sokhay Try <sokhay.try@gmail.com>
2023-12-12 14:05:11 +0700

    initial project

---
+++ src/main/java/com/springbootdaily/SpringBootDailyApplication.java
@@ -0,0 +1,13 @@
+package com.springbootdaily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDailyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDailyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootdaily/SpringBootDailyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootdaily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDailyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
